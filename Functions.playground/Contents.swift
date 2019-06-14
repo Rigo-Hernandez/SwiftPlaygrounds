82385abf3012ce2b9571bbe164e73c6044a3267f
Rigo Hernandez <rigohernandez@Rigos-iMac.attlocal.net>
2019-06-13 23:52:43 -0500

    Function practice recap

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,29 @@
+//Practice with simple functions
+
+//func getMilk () {
+//    print("Go to the shop")
+//    print("Buy 2 cartons of milk")
+//    print("Pay money")
+//    print("Come home")
+//}
+
+//getMilk()
+
+
+
+// Practice functions with inputs
+
+func getMilk (howManyMilkCartons : Int) {
+        print("Go to the shop")
+        print("Buy \(howManyMilkCartons) cartons of milk")
+    let priceToPay = howManyMilkCartons * 2
+    
+    
+        print("Pay \(priceToPay)")
+        print("Come home")
+}
+
+getMilk(howManyMilkCartons: 5)
+
+
+//Practice functions with outputs
