13b09a03d4a83d38076f67e9e66e7774b65f2312
Rigo Hernandez <rigohernandez@Rigos-iMac.attlocal.net>
2019-06-13 23:24:39 -0500

    first commit

---
+++ Debug.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// Basic understanding of how the print statement works.
+
+var monsterHealth = 19
+
+monsterHealth = monsterHealth + 22
+
+monsterHealth = monsterHealth / 2
+
+print(monsterHealth)
